94922328cbcd406c22ed6c04a583ce3bfb5de547
simpsonphile <simpsonphile@gmail.com>
2020-07-18 18:43:31 +0200

    Add content default

---
+++ components/ContentDefault.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    :class="classes"
+    class="t-content">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'content-default',
+    props: {
+        size: {
+            type: String,
+            default: 'm',
+            validator: prop => [
+                's',
+                'm',
+                'b'
+            ].includes(prop)
+        }
+    },
+    computed: {
+        classes () {
+            return [
+                this.size ? `t-content--s-${this.size}` : ''
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .t-content p {
+        margin: 0 0 2rem 0;
+
+        &:last-child {
+            margin: 0;
+        }
+    }
+
+    .t-content--s-m {
+        font-size: 1.6rem;
+    }
+</style>
