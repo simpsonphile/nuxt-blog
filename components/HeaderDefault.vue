f94ac965cbce19a24bede133ee9e8a3432c21cef
simpsonphile <simpsonphile@gmail.com>
2020-07-18 18:42:54 +0200

    Add header default

---
+++ components/HeaderDefault.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    :class="classes"
+    class="t-header">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'header-default',
+    props: {
+        size: {
+            type: String,
+            default: 'm',
+            validator: prop => [
+                's',
+                'm',
+                'b'
+            ].includes(prop)
+        }
+    },
+    computed: {
+        classes () {
+            return [
+                this.size ? `t-header--s-${this.size}` : ''
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .t-header--s-m:not(:last-child) {
+        margin-bottom: 1.5rem;
+    }
+
+    .t-header--s-m /deep/ .t-heading:not(:last-child) {
+        margin-bottom: .5rem;
+    }
+</style>
