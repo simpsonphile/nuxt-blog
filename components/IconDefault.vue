c6cf5a29e852e70f127993310b64bee36285c539
simpsonphile <simpsonphile@gmail.com>
2020-07-18 17:02:28 +0200

    Add icon component

---
+++ components/IconDefault.vue
@@ -0,0 +1,51 @@
+<template>
+    <span :class="classes">
+        <svg-icon :name="name" />
+    </span>
+</template>
+
+<script>
+export default {
+    name: 'icon',
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        size: {
+            type: String,
+            default: 'm',
+        },
+        color: {
+            type: String
+        }
+    },
+    computed: {
+        classes () {
+            return [
+                'o-ico',
+                this.name ? `o-ico--${this.name}` : '',
+                this.size ? `o-ico--s-${this.size}` : '',
+                this.color ? `u-paint-${this.color}` : ''
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .o-ico {
+        align-items: center;
+        justify-content: center;
+
+        display: flex;
+    }
+
+    .o-ico img,
+    .o-ico svg {
+        width: 100%;
+        height: 100%;
+
+        fill: currentColor;
+    }
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,8 @@ export default {
     '@nuxt/components'
   ],
   modules: [
-    '@nuxtjs/style-resources'
+    '@nuxtjs/style-resources',
+    '@nuxtjs/svg-sprite'
   ],
   styleResources: {
     scss: [
