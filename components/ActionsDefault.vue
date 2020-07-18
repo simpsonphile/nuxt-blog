3bf8734aed3fb890e7f3a9891320d9e72c2506d0
simpsonphile <simpsonphile@gmail.com>
2020-07-18 18:44:02 +0200

    Add actions object

---
+++ components/ActionsDefault.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="c-actions">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'actions-default'
+}
+</script>
+
+<style>
+    .c-actions {
+        margin-top: auto;
+        padding-top: 3rem;
+    }
+</style>
