3de1b2357db402c40428cac7644c75a3960d5de4
Dawid Mazewski <simpsonphile@gmail.com>
2020-08-01 20:07:06 +0200

    Add blog and category empty pages

---
+++ pages/blog/_category/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="s">strona kategorii</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ pages/blog/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="s">strona blog</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
