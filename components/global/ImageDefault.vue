4a55060adc29736bd763b2810c6da527a2ab1a84
Dawid Mazewski <simpsonphile@gmail.com>
2020-08-02 19:02:23 +0200

    Add global image object

---
+++ components/global/ImageDefault.vue
@@ -0,0 +1,19 @@
+<template>
+    <picture-default :src="image"></picture-default>
+</template>
+
+<script>
+export default {
+    name: 'image-default',
+    props: {
+        src: {
+            type: String
+        }
+    },
+    computed: {
+        image () {
+            return this.$images.get(this.src)
+        }
+    }
+}
+</script>
