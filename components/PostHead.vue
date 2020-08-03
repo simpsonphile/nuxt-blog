b932f0745f4eea676b017d330f5baf386164cc22
Dawid Mazewski <simpsonphile@gmail.com>
2020-08-03 20:14:12 +0200

    Add post head component

---
+++ components/PostHead.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="c-post-head">
+        <header-default>
+            <heading-default 
+                tag="h1"
+                size="l">
+                {{ title }}
+            </heading-default>
+        </header-default>
+
+        <picture-default
+            :src="image"
+            type="fit"
+            style="height: 300px"
+            :alt="title">
+        </picture-default>
+    </div>
+</template>
+
+<script>
+export default {
+	name: "post-head",
+    props: {
+        data: {
+            type: Object,
+            default: () => ({})
+        }
+    },
+    computed: {
+        title () {
+			return this.data.title
+		},
+        image () {
+            return this.data.image
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
